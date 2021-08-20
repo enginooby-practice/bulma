2672c9ca65adb13c1e3b07b7785bf383db0c687d
unknown <enginoobz@gmail.com>
2021-08-20 17:07:15 +0200

    Reformat project files due to new tab size

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-        const eNavbarBurger = document.querySelector(".navbar-burger")
-        const eNavbarMenu = document.querySelector(".navbar-menu")
+  const eNavbarBurger = document.querySelector(".navbar-burger")
+  const eNavbarMenu = document.querySelector(".navbar-menu")
 
-        eNavbarBurger.addEventListener('click', () => {
-                eNavbarMenu.classList.toggle('is-active')
-                eNavbarBurger.classList.toggle('is-active')
-        })
+  eNavbarBurger.addEventListener('click', () => {
+    eNavbarMenu.classList.toggle('is-active')
+    eNavbarBurger.classList.toggle('is-active')
+  })
 });
